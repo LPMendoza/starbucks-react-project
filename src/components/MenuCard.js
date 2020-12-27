8481e5d87c8f78c325616bc845cd72ab00551f8e
Pedro Mendoza <lpedromendoza333@gmail.com>
2020-12-27 00:34:08 -0600

    Products design changed

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 let MenuCard = ({ menuItem, handleOnClick}) => {
    return (
       <div onClick={(e) => handleOnClick(false)} className="col-12 col-md-6 col-lg-4 my-3 card-menu">
-         <div className="cont-menu-img shadow">
+         <div className="cont-menu-img ">
             <div className="imgMenuBackground" style={{ backgroundImage: `url(${menuItem.imgMenu})` }}>
             </div>
             <h1 className="text-white text-center menu-item-name"><span>{menuItem.menuName.trim()}</span></h1>
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -15,21 +15,21 @@ let Menu = () => {
    
    let showCtrlProducts = (e) => {
       return (
-         <div className="ctrlProducts my-md-3 mt-3 mb-0 col-12 px-0">
-            <div className="cont-backmenu col-12 col-lg-auto px-0">
+         <div className="ctrlProducts mt-md-4 mb-md-3 mb-0 col-12 px-0">
+            <div className="cont-backmenu col-12 col-md-auto px-0">
                   <button onClick={(e) => setMenuShowed(true)} 
-                  className="btn btn-primary rounded-pill pr-3 btnBackMenu text-white ">
+                  className="btn btn-primary rounded-pill pr-3 btnBackMenu text-white col-12 col-md-auto">
                      <span className="fas fa-arrow-left pr-3"></span>Regresar al menú
                   </button>
             </div>
-            <div className="cont-filter-menu mt-4 mb-5 my-lg-0 col-12 col-lg-auto px-0">
+            <div className="cont-filter-menu mt-4 mb-5 my-md-0 col-12 col-md-auto px-0">
 
-               <select className="pl-3 pr-5 col-12 col-lg-auto mb-2 mr-lg-3 mb-lg-auto rounded-pill bg-light">
-                  <option value="Relevante" defaultChecked>Relevante</option>
+               <select className="pl-3 pr-5 col-12 col-md-auto mb-2 mr-md-3 mb-md-auto rounded-pill bg-light">
+                  <option value="relevancia" defaultChecked>Relevancia</option>
                   <option value="upper">Mayor a menor precio</option>
                   <option value="lower">Menor a mayor precio</option>
                </select>
-               <div className="cont-search px-2 col-12 col-lg-auto rounded-pill bg-light">
+               <div className="cont-search px-2 col-12 col-md-auto rounded-pill bg-light">
                   <input type="text" className="ml-2" placeholder="Buscar..." required/>
                   <span class="fas fa-search mx-2"></span>
                </div>
@@ -59,18 +59,19 @@ let Menu = () => {
             <svg className="img-fluid wave-light" viewBox="0 0 1326 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                <path d="M1329 0H-2V50.2724C4.35961 50.2724 108.456 15.5883 205.357 15.5883C398.822 15.5883 416.73 79.5005 478.988 79.5005C541.245 79.5005 645.174 38.1914 768.685 38.1914C892.195 38.1914 882.154 66.2504 997.631 79.5005C1090.01 90.1005 1257.04 64.4318 1329 50.2724V0Z" fill="#F9F9F9" />
             </svg>
-            <h3 className="col-12 text-center mt-5 mb-lg-5 mb-3">
-               {menuShowed ? "SELECCIONA UNA CATEGORIA PARA VER SUS PRODUCTOS" : "BEBIDAS DE CALIDAD PARA TU DÍA"}</h3>
             <div className="container">
-               <div className="row  align-items-center justify-content-beetween px-3">
+               <div className="row align-items-center justify-content-beetween px-3">
+                  <h3 className="col-12 text-center mt-5 mb-lg-5 mb-4">
+                     {menuShowed ? "SELECCIONA UNA CATEGORIA PARA VER SUS PRODUCTOS" : "BEBIDAS DE CALIDAD PARA TI"}
+                  </h3>
                   {!menuShowed ? showCtrlProducts() : ""}
 
                </div>
             </div>
-            <svg className="img-fluid wave-circle" viewBox="0 0 1328 117" fill="none" xmlns="http://www.w3.org/2000/svg">
-               <path fillRule="evenodd" clipRule="evenodd" d="M664 0C901.192 0 1126.16 36.5404 1328 102L1328 118H0V102C201.836 36.5404 426.808 0 664 0Z" fill="#006241" />
-            </svg>
-            <article className="bg-primary article-menu pb-5">
+            {/* <svg className="img-fluid wave-circle" viewBox="0 0 1328 117" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path className="fill-gray-cream" fillRule="evenodd" clipRule="evenodd" d="M664 0C901.192 0 1126.16 36.5404 1328 102L1328 118H0V102C201.836 36.5404 426.808 0 664 0Z"/>
+            </svg> */}
+            <article className="bg-light-cream article-menu py-0 py-md-4">
                <div className="container">
                   <div className="row align-items-center justify-content-center">
                      <div className="cont-cards-menu col-12 px-0 d-flex flex-wrap">
